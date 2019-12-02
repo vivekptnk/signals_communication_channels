2021348e55a871dc2a4adeace043906d7afbbc88
vivekptnk <vivekpattanaik100@gmail.com>
2019-12-02 17:22:10 -0500

    Update communication_channels.m

--- communication_channels.m
+++ communication_channels.m
@@ -4,3 +4,21 @@ x = [0:1:13];
 stem(x,pulse);
 
 %partB
+rng(00708981);
+bits = randi([0 1],1,20);
+ind = [1:1:20];
+stem(ind,bits);
+B = bits;
+
+for i = 1:20
+    if B(i) == 0 
+        B(i) = -1;
+        i = i+1;
+    else
+        i = i+1;
+    end 
+end
+
+stem(ind,B);
+
+    
