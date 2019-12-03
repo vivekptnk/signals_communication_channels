70a6ab8e7c3aeb033a2a6854d86421a02af2e5ce
vivekptnk <vivekpattanaik100@gmail.com>
2019-12-02 19:05:35 -0500

    Update communication_channels.m

--- communication_channels.m
+++ communication_channels.m
@@ -1,7 +1,7 @@
 %partA
 pulse = [zeros(1,2),ones(1,10),zeros(1,2)];
-x = [0:1:13];
-stem(x,pulse);
+ind1 = [0:1:13];
+stem(ind1,pulse);
 
 %partB
 rng(00708981);
@@ -20,5 +20,29 @@ for i = 1:20
 end
 
 stem(ind,B);
+
+%partC
+negpulse = -pulse;
+x = []
+for i = 1:20 
+    if B(i) == 1
+        x = horzcat(x,pulse);
+        i = i + 1;
+    else 
+        x = horzcat(x,negpulse);
+        i = i + 1;
+    end
+end
+
+Fs = 10e3;
+t = [0:1/Fs:279/Fs];
+plot(t,x);
+        
+axis([-2/Fs 32/Fs -1.5 1.5]);
+
+
+%partD
+
+
 
     
